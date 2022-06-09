5a65088962bef74da5fef87a88a829f81fe82110
shc <sh@c>
2022-06-09 16:17:27 +0200

    Add entry-point for all src
    1.  Provide the package top-level __init__.py
    
    2.  Add the entry main.py for quick dependency testing

---
+++ src/main.py
@@ -0,0 +1,32 @@
+# The Tetris-Implementation of the Shetris-Project, written from scratch.
+#
+# Copyright (C) 2022 Shengdi 'shc' Chen (me@shengdichen.xyz)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+def numpy_test():
+    import numpy as np
+
+    print(2 * np.array([1, 2, 3]))
+
+
+def main():
+    print("Hello, world")
+    numpy_test()
+
+
+if __name__ == "__main__":
+    main()
