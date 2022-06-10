e90063ac92fc7cdc50f8927eb6f179ed3277ac53
shc <sh@c>
2022-06-10 17:03:46 +0200

    Add indexing from coordinates
    1.  numpy's advanced-indexing is triggered when indexing with an
        ndarray, the type of all coordinates of pieces
    
    2.  manual unpacking is thus required to access the entry

--- src/engine/placement/field.py
+++ src/engine/placement/field.py
@@ -21,6 +21,14 @@ import numpy as np
 
 
 class Field:
+    """
+    The field must be able to do:
+    1.  Indexing:
+        Give me coords,
+        Tell you the values there.
+
+    """
+
     def __init__(self, field: np.ndarray):
         self._field = field
         self._size = field.shape
@@ -48,6 +56,50 @@ class Field:
 
         # reset printoptions
         np.set_printoptions()
+
+    @staticmethod
+    def unpack_coord(coord: np.ndarray) -> tuple[int, int]:
+        """
+        Problem:
+            Accessing np.array with index of type np.array triggers np's
+            internal advanced-indexing.
+        Solution:
+            Manually unpack a coord (np.array); use this for access. This
+            routine performs this unpacking
+
+        :param coord: one np.array coord (two-entry, 1D)
+        :return: entry at the coord
+        """
+
+        return coord[0], coord[1]
+
+    def at_one(self, coord: np.ndarray) -> bool:
+        """
+        Get the entry of at one coordinate of np.ndarray.
+
+        :param coord: np.ndarray
+        :return: the entry at the coord
+        """
+        return self.field[Field.unpack_coord(coord)]
+
+    def at(self, coords: np.ndarray) -> np.ndarray:
+        """
+        Get the entries at many coordinates: a 2D np.ndarray:
+        1.  each is a coordinate
+        2.  i.e., dimension is n_coords*2
+
+        :param coords: the queried coordinates
+        :return: the entries at the queried coordinates
+        """
+        n_entries_queried = coords.shape[0]
+        states = np.empty(n_entries_queried, dtype=bool)
+
+        for i in range(n_entries_queried):
+            curr_coord = coords[i]
+            #   print(curr_coord)
+            states[i] = self.at_one(curr_coord)
+
+        return states
 
 
 if __name__ == "__main__":
