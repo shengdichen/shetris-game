25669a8a166bf4748579ac462f89aa40058d4d81
shc <sh@c>
2022-06-10 17:42:45 +0200

    Add check for exceeding of boundaries

--- src/engine/placement/field.py
+++ src/engine/placement/field.py
@@ -28,9 +28,18 @@ class Field:
         Tell you the values there.
     2.  Checks:
         Give me coords,
-        Tell you if there is collision.
+        Tell you if exceeded boundaries or has collision.
 
     """
+
+    # format:
+    # "check_string": [checking_in_pos0, checking_in_pos_dir]
+    check_strings_to_internals = {
+        "U": [True, False],
+        "D": [True, True],
+        "L": [False, False],
+        "R": [False, True],
+    }
 
     def __init__(self, field: np.ndarray):
         self._field = field
@@ -115,6 +124,75 @@ class Field:
         curr_vals = self.at(candidates)
 
         return np.any(curr_vals)
+
+    def _exceeded_boundary(
+        self, in_pos0: bool, in_pos_dir: bool, candidates: np.ndarray
+    ) -> np.bool_:
+        """
+        Check if any of the candidates-coordinates have exceeded one specific
+        boundary of the four "walls".
+
+        :param in_pos0: bool
+            True if in pos0 (check vertically);
+            False if in pos1 (check horizontally)
+        :param in_pos_dir: True if in negative direction, False otherwise
+        :param candidates: coordinates (2D!, we will extract the
+        relevant 1D coords in method!)
+
+        :return:
+        """
+
+        candidates_to_check = candidates[:, 0] if in_pos0 else candidates[:, 1]
+
+        if in_pos_dir:
+            np_checkers = (np.max, np.greater)
+            if in_pos0:
+                boundary = self.size[0] - 1
+            else:
+                boundary = self.size[1] - 1
+        else:
+            boundary = 0
+            np_checkers = (np.min, np.less)
+
+        def _check_util_1d(
+            _candidates_1d: np.ndarray, _boundary: int, _np_checkers: tuple
+        ) -> np.bool_:
+            """
+            check boundary against some boundary
+
+            :param _candidates_1d: 1D np.array (all rows or all cols)
+            :param _boundary:
+            :param _np_checkers:
+            :return:
+            """
+
+            worst_candidate = _np_checkers[0](_candidates_1d)
+            return _np_checkers[1](worst_candidate, _boundary)
+
+        return _check_util_1d(candidates_to_check, boundary, np_checkers)
+
+    def exceeded_boundaries(
+        self, boundaries_string: str, candidates: np.ndarray
+    ) -> bool:
+        """
+        Boundary-checks for multiple boundaries.
+
+        NOTE:
+        Short-circuiting is performed: as soon as one boundary-check is failed,
+        return immediately.
+
+        :param boundaries_string: any subset of "LRUD"
+        :param candidates:
+        :return:
+        """
+
+        for check_string in boundaries_string:
+            in_pos0, in_pos_dir = Field.check_strings_to_internals[check_string]
+            exceeded_curr = self._exceeded_boundary(in_pos0, in_pos_dir, candidates)
+            if exceeded_curr:
+                return True
+
+        return False
 
 
 if __name__ == "__main__":
