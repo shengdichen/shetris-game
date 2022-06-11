848a46400aa0d6b25d7e039549f765640935cdd8
shc <sh@c>
2022-06-11 03:58:36 +0200

    Add PoC for (smart) boundary&collision checks

--- src/engine/placement/mover.py
+++ src/engine/placement/mover.py
@@ -590,6 +590,53 @@ def test_setup():
     return m, piece
 
 
+def boundary_test():
+    m, piece = test_setup()
+    m.field.print_field()
+
+    # Right, within boundary
+    piece = Piece.to_absolute_pos(piece, np.array((0, 6)))
+    print(piece)
+    print(m.bad_boundaries_collision("DURL", piece))
+    # Right, OUTSIDE boundary
+    piece = Piece.to_absolute_pos(piece, np.array((0, 7)))
+    print(piece)
+    print(m.bad_boundaries_collision("DURL", piece))
+
+    # LEFT, within boundary
+    piece = Piece.to_absolute_pos(piece, np.array((5, 0)))
+    print(piece)
+    print(m.bad_boundaries_collision("DURL", piece))
+
+    # LEFT, OUTSIDE
+    piece = Piece.to_absolute_pos(piece, np.array((5, -1)))
+    print(piece)
+    print(m.bad_boundaries_collision("DURL", piece))
+
+    # NOTE:
+    # the following code demonstrates direct usage of boundary-checks, avoid if
+    # possible as the boundary-collision check short-circuits with the
+    # boundary-checks performed first anyway
+    #
+    # # Up, within boundary
+    # Piece.to_absolute_pos(piece, np.array((-1, 0)))
+    # print(piece)
+    # print(m._bad_boundary(piece, True, False))
+    # # Up, OUTSIDE boundary
+    # piece = Piece.to_absolute_pos(piece, np.array((-2, 0)))
+    # print(piece)
+    # print(m._bad_boundary(piece, True, False))
+    #
+    # # Down, within boundary
+    # piece = Piece.to_absolute_pos(piece, np.array((18, 0)))
+    # print(piece)
+    # print(m._bad_boundary(piece, True, True))
+    # # Up, OUTSIDE boundary
+    # piece = Piece.to_absolute_pos(piece, np.array((19, 0)))
+    # print(piece)
+    # print(m._bad_boundary(piece, True, True))
+
+
 def atomic_test():
     m, piece = test_setup()
     m.field.print_field()
